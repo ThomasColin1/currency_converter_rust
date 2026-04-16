c5d855ed9ab590f9d6a66c2005cfdf63175eae95
colin <colin.thomas2@protonmail.com>
2026-04-16 22:18:23 +0200

    update only needed values, every 60 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,76 @@
+//TODO : manage parallel requests (tokio)
+//TODO : make a simple TUI
+
 use std::collections::HashMap;
 
-
-#[derive(PartialEq, Eq, Hash, Debug)]
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
 enum Currency{
     EUR,
     USD,
     JPY
 }
 
+struct CurrencyValueTuple {
+    price: f64,
+    last_updated: std::time::Instant
+}
+
 struct CurrencyConverter{
-    currencies: HashMap<Currency, f64>
+    currencies: HashMap<Currency, CurrencyValueTuple>
 }
 
 impl CurrencyConverter {
     pub fn new() -> Self {
-        let currencies = HashMap::from([
-            (Currency::EUR, 0.5),
-            (Currency::USD, 1.0),
-            (Currency::JPY, 2.0),
-        ]);
-        Self { currencies }
+        Self { currencies: HashMap::new() }
+
     }
-    pub fn update_from_web(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    fn refresh_currency(&mut self, code: Currency) -> Result<(), Box<dyn std::error::Error>> {
+        //TODO : no repetition ?
+        let symbol = match code {
+            Currency::EUR => "EUR",
+            Currency::USD => "USD",
+            Currency::JPY => "JPY",
+        };
+
         let url = "https://api.exchangerate-api.com/v4/latest/USD";
         let json: serde_json::Value = reqwest::blocking::get(url)?.json()?;
 
-        if let Some(eur) = json["rates"]["EUR"].as_f64() {
-            self.currencies.insert(Currency::EUR, eur);
+        if let Some(price) = json["rates"][symbol].as_f64() {
+            self.currencies.insert(code, CurrencyValueTuple {
+                price,
+                last_updated: std::time::Instant::now(),
+            });
         }
-        if let Some(usd) = json["rates"]["USD"].as_f64() {
-            self.currencies.insert(Currency::USD, usd);
-        }
-        if let Some(jpy) = json["rates"]["JPY"].as_f64() {
-            self.currencies.insert(Currency::JPY, jpy);
-        }
-
         Ok(())
     }
-    pub fn convert(&self, from: &Currency, to: &Currency, quantity: f64) 
+    pub fn convert(&mut self, from: &Currency, to: &Currency, quantity: f64) 
         -> Option<f64> {
-        let from_rate = self.currencies.get(from)?;
-        let to_rate = self.currencies.get(to)?;
+        if self.is_dated(from) {
+            //TODO : manage error
+            let _ = self.refresh_currency(*from);
+        }
+    
+        if self.is_dated(to) {
+            //TOOD : manage error
+            let _ = self.refresh_currency(*to);
+        }
+        let from_rate = self.currencies.get(from)?.price;
+        let to_rate = self.currencies.get(to)?.price;
 
         Some(quantity/from_rate*to_rate)
+    }
+    fn is_dated(&self, code: &Currency) -> bool {
+        match self.currencies.get(code) {
+            None => true, 
+            Some(entry) => entry.last_updated.elapsed() > std::time::Duration::from_secs(60),
+        }
     }
 }
 
 
-
 fn main() {
     let mut converter = CurrencyConverter::new();
-
-    let quantity = 100.00;
-    let from = Currency::EUR;
-    let to = Currency::USD;
-
-    if let Err(e) = converter.update_from_web() {
-        println!("Error while updating from API : {}", e);
-    } else {
-        match converter.convert(&from, &to, quantity) {
-            Some(result) => {
-                println!("{quantity} {:?} is {result:.2} {:?}", from , to);
-            }
-            None => println!("Error: unsupported currency."),
-        }
-    }
-
+    println!("Conversion 1 : EUR to USD : {:?}", converter.convert(&Currency::EUR, &Currency::USD, 100.0));
+    println!("Conversion 2 : EUR to USD : {:?}", converter.convert(&Currency::EUR, &Currency::USD, 100.0));
+    println!("Conversion 3 : EUR to JPY : {:?}", converter.convert(&Currency::EUR, &Currency::JPY, 100.0));
 }
